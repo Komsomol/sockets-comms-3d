eb226a0936a6cb6388a7cc5572d67bbf07ceed46
Farhad Agzamov <farhad.agza@gmail.com>
2023-10-21 17:39:02 +0100

    Intial Files

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new Server({ server });
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        const receivedMessage = message.toString();
+        console.log('Received:', receivedMessage);
+
+        // Broadcast the message to all connected clients
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === ws.OPEN) {  // Change this line
+                client.send(receivedMessage);
+            }
+        });
+    });
+
+    ws.send('Connection established!');
+});
+
+const PORT = 3001;
+server.listen(PORT, () => {
+    console.log(`WebSocket server running on ws://localhost:${PORT}`);
+});
