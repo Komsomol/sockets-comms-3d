b0230b516a95df668178ab8f485d552d076271d6
Farhad Agzamov <farhad.agza@gmail.com>
2023-10-25 21:29:59 +0100

    Added models and loader

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
-import * as THREE from "three";
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+// scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
 	75,
@@ -8,22 +11,64 @@ const camera = new THREE.PerspectiveCamera(
 	1000
 );
 
+// renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
-
+// sockets
 const ws = new WebSocket("ws://localhost:3001");
 ws.onopen = () => {
 	console.log("Connected to WebSocket server.");
 };
 
+// camera
+camera.position.x = -6;
+camera.position.y = 6;
+camera.position.z = 6;
+// Attach camera to the window object
+window.camera = camera;
+
+// orbit controls
+const orbitControls = new OrbitControls( camera, renderer.domElement );
+
+// gltf
+const loader = new GLTFLoader();
+let model;
+let loaded = false;
+loader.load( './model/london_eye_london_uk.glb', function ( gltf ) {
+	loaded = true;
+	// scene.add( gltf.scene );
+	model = gltf.scene;
+	modelloaded();
+}, undefined, function ( error ) {
+	console.error( error );
+} );
+
+function modelloaded() {
+	if (loaded) {
+		scene.add( model );
+	}
+}
+
+
+// light
+const ambilight = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( ambilight );
+
+const hemilight = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+scene.add( hemilight );
+const helper = new THREE.HemisphereLightHelper( hemilight, 5 );
+scene.add( helper );
+
+// helpers
+const gridHelper = new THREE.GridHelper( 100, 100 );
+scene.add( gridHelper );
+
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
+
+// animate
 function animate() {
 	requestAnimationFrame(animate);
 
@@ -31,10 +76,15 @@ function animate() {
 		const command = event.data;
 		switch (command) {
 			case "rotate-left":
-				cube.rotation.y -= 0.1;
+				if(loaded){
+					model.rotation.y -= 0.1;
+				}
+
 				break;
 			case "rotate-right":
-				cube.rotation.y += 0.1;
+				if(loaded){
+					model.rotation.y += 0.1;
+				}
 				break;
 		} // Render the scene after rotating the cube
 	};
